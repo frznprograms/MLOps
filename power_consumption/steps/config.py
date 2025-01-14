7a458d047cbc03394af5a33020e18e7861e2d6d6
Shane <112380670+frznprograms@users.noreply.github.com>
2025-01-14 23:19:09 +0800

    Update config.py

--- power_consumption/steps/config.py
+++ power_consumption/steps/config.py
@@ -1,14 +1,14 @@
-from zenml.steps import BaseParameters
+from pydantic import BaseModel
 
-class ModelConfig(BaseParameters):
-    model_task: str = "reg"
+class ModelConfig(BaseModel):
+    task: str = "reg"
 
-class RegConfig(BaseParameters):
+class RegConfig(BaseModel):
     pos_coeff: list = [False, True]
-    random_state: int = 1
+    random_state: list = [1]
 
-class ClusterConfig(BaseParameters):
+class ClusterConfig(BaseModel):
     n_clusters: list = [5, 8, 10]
     max_iter: list = [300, 400, 500]
     tol: list = [0.005, 0.001, 0.0005]
-    random_state: int = 1
+    random_state: list = [1]
