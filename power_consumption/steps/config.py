9e586a9b2b109acc05927c0c1e696db651d0a853
Shane <112380670+frznprograms@users.noreply.github.com>
2025-01-15 22:03:37 +0800

    Update config.py

--- power_consumption/steps/config.py
+++ power_consumption/steps/config.py
@@ -5,10 +5,8 @@ class ModelConfig(BaseModel):
 
 class RegConfig(BaseModel):
     pos_coeff: list = [False, True]
-    random_state: list = [1]
 
 class ClusterConfig(BaseModel):
     n_clusters: list = [5, 8, 10]
     max_iter: list = [300, 400, 500]
     tol: list = [0.005, 0.001, 0.0005]
-    random_state: list = [1]
